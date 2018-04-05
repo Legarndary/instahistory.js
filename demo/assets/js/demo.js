712d971618df7681bc417ec463e4ee98ad091f9a
Kasper Legarth <kasperlegarth@gmail.com>
2018-04-05 08:39:05 +0200

    Small fixes

--- demo/assets/js/demo.js
+++ demo/assets/js/demo.js
@@ -1,6 +1,6 @@
 $('#feed').instahistory({
-    get: '#summer',
+    get: '@instagram',
     imageSize: 240,
     limit: 9,
-    template: '<div class="col"><a href="{{link}}" type="_blank"><img src="{{image}}"><span class="info">💗 {{likes}} 💬 {{comments}}</span></a></div>'
+    template: '<div class="col"><a href="{{link}}" type="_blank"><img src="{{image}}"><span class="info">❤️ {{likes}} 💬 {{comments}}</span></a></div>'
 });
